d9e678f4479c2cc23343429d48e94493d97c0520
Edwin <edoumacodu@gmail.com>
2022-10-08 13:10:43 +0300

    added login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1> Consume StudentAPI </h1>
-    </div>
+      <Navbar/>
+      <div className="content"> 
+        <Switch>
+          <Route exact path="/">
+            
+          </Route>    
+
+          <Route path="/login">
+            <Login /> 
+          </Route> 
+
+          <Route path="/blogs/:id">
+             
+          </Route>  
+
+          <Route path="*">
+            {/* <NotFound />  */}
+          </Route>  
+            
+        </Switch>
+        </div>
+        
+      </div>
+      </Router>
   );
 }
 
